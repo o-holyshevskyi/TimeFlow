ab40a6b9b458a3e45795f262fe3c55a149da9980
oholyshevskyi <oleksandr.holyshevskyi@pcmicorp.com>
2025-12-06 21:35:59 +0100

    improve layout of the add

--- timeflow/components/advertisment/ad-banner.tsx
+++ timeflow/components/advertisment/ad-banner.tsx
@@ -1,8 +1,9 @@
 import { useRef } from 'react';
-import { View } from 'react-native';
+import { Dimensions, View } from 'react-native';
 import { BannerAd, BannerAdSize, TestIds, useForeground } from 'react-native-google-mobile-ads';
 
 const adUnitId = TestIds.BANNER;
+const AD_WIDTH = Dimensions.get('window').width * .80;
 
 const AdBanner = ({ isPremiumUser = false }: { isPremiumUser: boolean }) => {
     const bannerRef = useRef<BannerAd>(null);
@@ -14,10 +15,10 @@ const AdBanner = ({ isPremiumUser = false }: { isPremiumUser: boolean }) => {
     if (isPremiumUser) return null;
     
     return (
-        <View style={{ width: '100%', alignItems: 'center', marginVertical: 10 }}>
+        <View style={{ width: AD_WIDTH, height: 100, alignItems: 'center', marginVertical: 10 }}>
             <BannerAd
                 unitId={adUnitId} 
-                size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+                size={BannerAdSize.FULL_BANNER}
                 requestOptions={{
                     requestNonPersonalizedAdsOnly: true,
                 }}
